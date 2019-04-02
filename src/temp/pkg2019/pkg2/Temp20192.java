950c0a2d1ffd58a7c964b296c9bb402324c520a7
fabian121 <fabian121@gmail.com>
2019-04-02 09:12:04 -0600

    Update Temp20192.java

--- src/temp/pkg2019/pkg2/Temp20192.java
+++ src/temp/pkg2019/pkg2/Temp20192.java
@@ -17,6 +17,7 @@ public class Temp20192 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello world!");
     }
     
 }
